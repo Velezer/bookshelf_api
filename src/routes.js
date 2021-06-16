303c41bf7894006d7d260716fb5e21f2e801a855
Arief Syaifuddin <asvelezer@gmail.com>
2021-06-17 00:22:04 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+const { addBookHandler, getAllBooksHandler, getBookById, editBookByIdHandler, deleteBookById } = require("./handler")
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    }, {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler
+    }, {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookById
+    }, {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookByIdHandler
+    }, {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookById
+    },
+]
+
+module.exports = routes
